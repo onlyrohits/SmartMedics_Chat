50d8165a1dacfefc4efaf79e671354472823be3c
piyushonkar <36102025+piyushonkar@users.noreply.github.com>
2018-05-15 18:45:21 +0530

    Add files via upload
    Added some new extra features like deleting the elements which are used for extraction of age.

--- age.py
+++ age.py
@@ -31,54 +31,66 @@ def age_extraction(words):
 			if i+4 < size:
 				if words[i+1]=='year' and (words[i+2]!='and' or words[i+3]!='month'):
 					age=age_temp
+					number_index=[i,i+1]
 					break
 				elif words[i+1]=='month':
 					age=age_temp/12
+					number_index=[i,i+1]
 					break
 				elif words[i+1]=='year' and words[i+2]=='and' and words[i+4]=='month':
 					try:
 						month_temp=float(words[i+3])
 						age = age + month_temp/12
+						number_index=[i,i+1,i+2,i+3,i+4]
 						break
 					except ValueError:
 						month_temp=text2int(words[i+3])
 						age = age + month_temp/12
+						number_index=[i,i+1,i+2,i+3,i+4]
 						break
 				elif words[i+1]=='year' and words[i+2]!='and' and words[i+3]=='month':
 					try :
-						month_temp=float(words[i+3])
+						month_temp=float(words[i+2])
 						age = age + month_temp/12
+						number_index=[i,i+1,i+2,i+3]
 						break
 					except ValueError:
 						month_temp=text2int(words[i+2])
 						age= age + month_temp/12
+						number_index=[i,i+1,i+2,i+3]
 						break
 				else:
 					continue
 			elif i+3 < size :
 				if words[i+1]=='year' and (words[i+2]!='and' or words[i+3]!='month'):
 					age=age_temp
+					number_index=[i,i+1]
 					break
 				elif words[i+1]=='month':
 					age=age_temp/12
+					number_index=[i,i+1]
 					break
 				elif words[i+1]=='year' and words[i+2]!='and' and words[i+3]=='month':
 					try :
-						month_temp=float(words[i+3])
+						month_temp=float(words[i+2])
 						age = age + month_temp/12
+						number_index=[i,i+1,i+2,i+3]
 						break
 					except ValueError:
 						month_temp=text2int(words[i+2])
 						age= age + month_temp/12
+						number_index=[i,i+1,i+2,i+3]
 						break
 				else:
 					continue
 			elif i+1 < size :
 				if words[i+1]=='year':
 					age=age_temp
+					number_index=[i,i+1]
 					break
 				elif words[i+1]=='month':
 					age=age_temp/12
+					number_index=[i,i+1]
 					break
 				else:
 					continue
@@ -91,54 +103,66 @@ def age_extraction(words):
 					if i+5 < size:
 						if words[i+2]=='year' and (words[i+3]!='and' or words[i+3]!='month'):
 							age=age_temp + age_temp1
+							number_index=[i,i+1,i+2]
 							break
 						elif words[i+2]=='month':
 							age=(age_temp+age_temp1)/12
+							number_index=[i,i+1,i+2]
 							break
 						elif words[i+2]=='year' and words[i+3]=='and' and words[i+5]=='month':
 							try:
 								month_temp=float(words[i+4])
 								age = age_temp + age_temp1 + month_temp/12
+								number_index=[i,i+1,i+2,i+3,i+4,i+5]
 								break
 							except ValueError:
 								month_temp=text2int(words[i+4])
 								age = age_temp + age_temp1 + month_temp/12
+								number_index=[i,i+1,i+2,i+3,i+4,i+5]
 								break
 						elif words[i+2]=='year' and words[i+3]!='and' and words[i+4]=='month':
 							try :
 								month_temp=float(words[i+3])
 								age = age_temp1 + age_temp + month_temp/12
+								number_index=[i,i+1,i+2,i+3,i+4]
 								break
 							except ValueError:
 								month_temp=text2int(words[i+3])
 								age= age_temp + age_temp1 + month_temp/12
+								number_index=[i,i+1,i+2,i+3,i+4]
 								break
 						else:
 							continue
 					if i+4 < size :
 						if words[i+2]=='year' and (words[i+3]!='and' or words[i+3]!='month'):
 							age=age_temp + age_temp1
+							number_index=[i,i+1,i+2]
 							break
 						elif words[i+2]=='month':
 							age=(age_temp+age_temp1)/12
+							number_index=[i,i+1,i+2]
 							break
 						elif words[i+2]=='year' and words[i+3]!='and' and words[i+4]=='month':
 							try :
 								month_temp=float(words[i+3])
 								age = age_temp1 + age_temp + month_temp/12
+								number_index=[i,i+1,i+2,i+3,i+4]
 								break
 							except ValueError:
 								month_temp=text2int(words[i+3])
 								age= age_temp + age_temp1 + month_temp/12
+								number_index=[i,i+1,i+2,i+3,i+4]
 								break
 						else:
 							continue
 					if i+2 < size:
 						if words[i+2]=='year' :
 							age=age_temp + age_temp1
+							number_index=[i,i+1,i+2]
 							break
 						elif words[i+2]=='month':
 							age=(age_temp+age_temp1)/12
+							number_index=[i,i+1,i+2]
 							break
 						else:
 							break
@@ -146,61 +170,73 @@ def age_extraction(words):
 					if i+4<size:
 						if words[i+1]=='year' and (words[i+2]!='and' or words[i+3]!='month'):
 							age=age_temp
+							number_index=[i,i+1]
 							break
 						elif words[i+1]=='month':
 							age=age_temp/12
+							number_index=[i,i+1]
 							break
 						elif words[i+1]=='year' and words[i+2]=='and' and words[i+4]=='month':
 							try:
 								month_temp=float(words[i+3])
 								age = age + month_temp/12
+								number_index=[i,i+1,i+2,i+3,i+4]
 								break
 							except ValueError:
 								month_temp=text2int(words[i+3])
 								age = age + month_temp/12
+								number_index=[i,i+1,i+2,i+3,i+4]
 								break
 						elif words[i+1]=='year' and words[i+2]!='and' and words[i+3]=='month':
 							try :
 								month_temp=float(words[i+2])
 								age = age + month_temp/12
+								number_index=[i,i+1,i+2,i+3]
 								break
 							except ValueError:
 								month_temp=text2int(words[i+2])
 								age= age + month_temp/12
+								number_index=[i,i+1,i+2,i+3]
 								break
 						else:
 							continue
 					elif i+3<size:
 						if words[i+1]=='year' and (words[i+2]!='and' or words[i+3]!='month'):
 							age=age_temp
+							number_index=[i,i+1]
 							break
 						elif words[i+1]=='month':
 							age=age_temp/12
+							number_index=[i,i+1]
 							break
 						elif words[i+1]=='year' and words[i+2]!='and' and words[i+3]=='month':
 							try :
 								month_temp=float(words[i+2])
 								age = age + month_temp/12
+								number_index=[i,i+1,i+2,i+3]
 								break
 							except ValueError:
 								month_temp=text2int(words[i+2])
 								age= age + month_temp/12
+								number_index=[i,i+1,i+2,i+3]
 								break
 						else:
 							continue
 					elif i+1<size:
 						if words[i+1]=='year':
 							age=age_temp
+							number_index=[i,i+1]
 							break
 						elif words[i+1]=='month':
 							age=age_temp/12
+							number_index=[i,i+1]
 							break
 					else:
 						continue
 
 			except :
 				continue
-	return age
+	return age,number_index
 
 ##############################################################################################
 
@@ -238,11 +274,14 @@ def text2int(textnum, numwords={}):
 
     return result + current
 
+##########################################################################################################
+
 
 
 ###############################################################################################
 
-description=input("Enter String: ")
+description = input("Enter the string : ")
+description=description.lower()
 lemma=WordNetLemmatizer()
 temp=word_tokenize(description)
 temp_words=[]
@@ -250,7 +289,6 @@ for m in temp:
 	temp_words.append(lemma.lemmatize(m))
 word_list=parsing(temp_words)
 print(word_list)
-
 ##################################
 
 male=[]
@@ -275,6 +313,19 @@ for sex in word_list:
 
 #######################################################
 
-age=age_extraction(word_list)
-print(age)
+cleaned_list=word_list ##cleaned_list is list obtained after removing elements from indices
+age,indices=age_extraction(word_list)
+
+##########################################################################
+
+length_indices=len(indices)
+first_element=indices[0]
+last_element=indices[length_indices-1]
+del(cleaned_list[first_element:last_element+1])
+
+#######################################################################################
+
+print(cleaned_list)
+
+
 
