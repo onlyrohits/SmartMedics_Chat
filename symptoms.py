43a402c0b1b463998b7bc3ceab00041a34e95d10
piyushonkar <36102025+piyushonkar@users.noreply.github.com>
2018-05-17 17:07:03 +0530

    Add files via upload
    Symptoms and specialist  can  be extracted now.

--- symptoms.py
+++ symptoms.py
@@ -4,7 +4,8 @@ from nltk.stem import WordNetLemmatizer
 import pandas as pd
 import csv
 
-
+#####################################################################################
+'''
 # Function to remove stopwords and clean the string
 def parsing(word):
 	words=[]
@@ -20,6 +21,97 @@ def parsing(word):
 					words.append(element)
 	return words
 
+'''
+
+################################################################################################
+'''
+def symptom_specialist(words,dict_list):
+	sym=[]
+	special=[]
+	word_length=len(words)
+	for i in range (0,word_length):
+		for j in dict_list:
+			if i+4 < word_length:
+				if words[i] in j['symptoms']:
+					if words[i+1] in j['symptoms']:
+						if words[i+2] in j['symptoms']:
+							if words[i+3] in j['symptoms']:
+								if words[i+4] in j['symptoms']:
+									sym.append(words[i] + ' ' + words[i+1]+ ' ' + words[i+2] + ' ' + words[i+3]+ ' ' +words[i+4])
+									special.append(j['specialist'])
+								else:
+									sym.append(words[i]+ " " + words[i+1]+ " " + words[i+2]+ " " + words[i+3])
+									special.append(j['specialist'])
+							else:
+								sym.append(words[i]+ " " +words[i+1]+ " " +words[i+2])
+								special.append(j['specialist'])
+						else:
+							sym.append(words[i]+ " " +words[i+1])
+							special.append(j['specialist'])
+					else:
+						if words[i]==j['symptoms']:
+							sym.append(words[i])
+							special.append(j['specialist'])
+			elif i+3 < word_length:
+				if words[i] in j['symptoms']:
+					if words[i+1] in j['symptoms']:
+						if words[i+2] in j['symptoms']:
+							if words[i+3] in j['symptoms']:
+								sym.append(words[i]+ " " +words[i+1]+ " " +words[i+2]+ " " +words[i+3])
+								special.append(j['specialist'])
+							else:
+								sym.append(words[i]+ " " +words[i+1]+ " " +words[i+2])
+								special.append(j['specialist'])
+						else:
+							sym.append(words[i]+ " " +words[i+1])
+							special.append(j['specialist'])
+					else:						
+						if words[i]==j['symptoms']:
+							sym.append(words[i])
+							special.append(j['specialist'])
+			elif i+2 < word_length:
+				if words[i] in j['symptoms']:
+					if words[i+1] in j['symptoms']:
+						if words[i+2] in j['symptoms']:
+							sym.append(words[i]+ " " +words[i+1]+ " " +words[i+2])
+							special.append(j['specialist'])
+						else:
+							sym.append(words[i]+ " " +words[i+1])
+							special.append(j['specialist'])
+					else:
+						if words[i]==j['symptoms']:
+							sym.append(words[i])
+							special.append(j['specialist'])
+			elif i+1 < word_length:
+				if words[i] in j['symptoms']:
+					if words[i+1] in j['symptoms']:
+						sym.append(words[i]+ " " +words[i+1])
+						special.append(j['specialist'])
+					else:
+						if words[i]==j['symptoms']:
+							sym.append(words[i])
+							special.append(j['specialist'])
+			else:
+				if words[i] in j['symptoms']:
+					sym.append(words[i])
+					special.append(j['specialist'])
+
+	return (sym,special)
+	'''
+
+#############################################################################################
+
+def symptom_specialist(words,dict_list):
+	sym=[]
+	special=[]
+	string=" ".join(words)
+	for i in dict_list:
+		if i['symptoms'] in string:
+			sym.append(i['symptoms'])
+			special.append(i['specialist'])
+	return (sym,special)
+
+###############################################################################################
 
 # To take a string as input and tokenise, lemmatize and conversion to lower case
 sentence=input('Enter a string:')
@@ -29,9 +121,10 @@ temp=word_tokenize(sentence)
 temp_words=[]
 for m in temp:
 	temp_words.append(lemma.lemmatize(m))
-words=parsing(temp_words)
-print (words)
 
+words=temp_words
+
+##################################################################################################
 
 # To read the CSV file as a list of dictionaries
 reader=csv.DictReader(open('symptoms.csv','r'),fieldnames=('specialist','symptoms'))
@@ -39,20 +132,17 @@ dict_list=[]
 for line in reader:
 	dict_list.append(line)
 
+####################################################################
 
 # To check if the symptoms match those in the list of dictionaries and store it in another list sym[] and store corresponding specialists in special=[]
-sym=[]
-special=[]
-for i in words:
-	for j in dict_list:
-		if i==j['symptoms']:
-			sym.append(i)
-			special.append(j['specialist'])
-print(sym)
+symptm,special=symptom_specialist(words,dict_list)
+print(symptm,special)
+
+'''
+# To convert the results into percentages
+for i in special:
+	i=i.lower() #only to convert matching strings to same format
 print(special)
-
-
-# To convert the results into numerical data for analysis
 count={} #will work as a flag to count how many times a specialist has been recommended
 for i in special:
 	if i in count.keys():
@@ -61,15 +151,4 @@ for i in special:
 		count[i]=1
 print(count)
 
-# To calculate the percentage
-total_flags=sum(count.values())
-percent=count.fromkeys(count.keys())
-for i in percent:
-	percent[i]=(count[i]/total_flags)*100
-print(percent)
-only_specs=count.keys() # Saved the list of specialists
-new_list=[]
-for i in only_specs:
-	new_list.append(i)
-for key,value in percent.items():
-	print('There is '+str(value)+' percent chance that you need to see a '+key)
+'''
