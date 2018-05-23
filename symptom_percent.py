42cd1c6363de7a16d33b5e91b79d5e5029cba1d6
piyushonkar <36102025+piyushonkar@users.noreply.github.com>
2018-05-23 20:04:20 +0530

    Add files via upload
    Can predict specialist based on symptoms and is dependent on 123.csv

---
+++ symptom_percent.py
@@ -0,0 +1,35 @@
+
+
+
+
+sentence=input("Describe your problem: ")
+
+filepath = '123.csv'
+master_list=[]
+with open(filepath) as fp:  
+   line = fp.readline()
+   while line:
+       stripped_line=line.strip()
+       strip_list=stripped_line.split(",")
+       master_list.append(strip_list)
+       line = fp.readline()
+
+sum_list={}
+
+for element in master_list:
+	if sentence.find(element[1]) >= 0 :
+		if not (element[0] in sum_list.keys()):
+			sum_list[element[0]]=float(element[2])
+		else:
+			value=sum_list[element[0]]+float(element[2])
+			sum_list[element[0]]=value
+
+total=0
+for value in sum_list.values():
+	total = total + value
+
+for key in sum_list.keys():
+	sum_list[key] = sum_list[key]*100/total
+
+for key,value in sum_list.items():
+	print("The probability of going to {} is {} percent".format(key,value))
