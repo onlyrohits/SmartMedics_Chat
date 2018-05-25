d82976ec622d5078087906ea5b4fcdfabdf92e79
piyushonkar <36102025+piyushonkar@users.noreply.github.com>
2018-05-25 12:51:23 +0530

    Add files via upload
    The code is working- documentation need to be done

--- symptom_percent.py
+++ symptom_percent.py
@@ -1,5 +1,6 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import pandas as pd
 from autocorrect import spell
 
 #################################################################
@@ -36,7 +37,7 @@ def correction(sentence,List):
 	x=" ".join(words)
 	return(x)
 
-##########################################################################
+########################################################################################################
 
 sentence=input("Describe your problem: ")
 sentence=sentence.lower()
@@ -61,64 +62,87 @@ with open(filepath) as fp:
 
 Sym_list=list(set(Sym_list))	
 sentence=correction(sentence,Sym_list)
+
+
 print(sentence)
 
-sum_list={}
+###############################################################################################
 
+male=[]
+female=[]
+filename='MaleFemale.csv'
+dataset=pd.read_csv(filename)
+array=dataset.values
+for i in array:
+	male.append(i[0])
+	female.append(i[1])
+
+gender=''
+for sex in word_tokenize(sentence):
+	if sex in male:
+		gender='Male'
+		break
+	elif sex in female:
+		gender='Female'
+		break
+
+#####################################################################################
+
+sum_list={}
 dominant_key=''
 psych_present=''
-primary=['sex', 'cancer', 'eye']
+primary=['sex', 'cancer', 'eye','abortion']
 primary_indices={}
+
 for m in primary:
 	if sentence.find(m)>=0:
 		primary_indices[m]=sentence.find(m)
-	else:
 
 min_key=''
+
 for key,value in primary_indices.items():
 	if primary_indices[key]==min(primary_indices.values()):
 		min_key=key
 
 if min_key=='sex':
-	if gender=='male':
-		if sentence.find('hypertension')>=0 or sentence.find('anxiety')>=0 or sentence.find('stress')>=0 or sentence.find('depression')>=0 :
-			dominant_key='sexologist'
-			psych_present='psychiatrist'
-			print('There is a high probability that you should consult a '+dominant_key+' and '+psych_present)
-	else:
+	if gender=='Female':
 		if sentence.find('hypertension')>=0 or sentence.find('anxiety')>=0 or sentence.find('stress')>=0 or sentence.find('depression')>=0 :
 			dominant_key='gynaecologist'
 			psych_present='psychiatrist'
 			print('There is a high probability that you should consult a '+dominant_key+' and '+psych_present)
-
-if min_key=='cancer':
-	dominant_key='oncologist'
-	print('There is a high probability that you should consult a '+dominant_key)
-if min_key=='eye':
-	dominant_key='opthalmologist'
-	print('There is a high probability that you should consult a '+dominant_key)
-
-
-
-for element in master_list:
-	if sentence.find(element[1]) >= 0 :
-		if not (element[0] in sum_list.keys()):
-			sum_list[element[0]]=float(element[2])
-			print(element[1])
-			print(element[0])
 		else:
-			value=sum_list[element[0]]+float(element[2])
-			sum_list[element[0]]=value
-			print(element[1])
-			print(element[0])
+			dominant_key='gynaecologist'
+			print('There is a high probability that you should consult a '+dominant_key)
+	else:
+		if sentence.find('hypertension')>=0 or sentence.find('anxiety')>=0 or sentence.find('stress')>=0 or sentence.find('depression')>=0 :
+			dominant_key='sexologist'
+			psych_present='psychiatrist'
+			print('There is a high probability that you should consult a '+dominant_key+' and '+psych_present)
+		else:
+			dominant_key='sexologist'
+			print('There is a high probability that you should consult a '+dominant_key)
 
-total=0
-
-for value in sum_list.values():
-	total = total + value
-
-for key in sum_list.keys():
-	sum_list[key] = sum_list[key]*100/total
-
-for key,value in sum_list.items():
-	print("The probability of going to {} is {} percent".format(key,value))
+elif min_key=='cancer':
+	dominant_key='oncologist'
+	print('There is a high probability that you should consult a '+ dominant_key)
+elif min_key=='eye':
+	dominant_key='opthalmologist'
+	print('There is a high probability that you should consult a '+ dominant_key)
+elif 'abortion' in 'sentence':
+	dominant_key='gynaecologist'
+	print('There is a high probability that you should consult a '+ dominant_key)
+else:
+	for element in master_list:
+		if sentence.find(element[1]) >= 0 :
+			if not (element[0] in sum_list.keys()):
+				sum_list[element[0]]=float(element[2])
+			else:
+				value=sum_list[element[0]]+float(element[2])
+				sum_list[element[0]]=value
+	total=0
+	for value in sum_list.values():
+		total = total + value
+	for key in sum_list.keys():
+		sum_list[key] = sum_list[key]*100/total
+	for key,value in sum_list.items():
+		print("The probability of going to {} is {} percent".format(key,value))
