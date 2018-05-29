d57a262051dbd41cf3ebe5bf2893e8ea10e6e33d
piyushonkar <36102025+piyushonkar@users.noreply.github.com>
2018-05-29 17:01:51 +0530

    Add files via upload
    Need to integrate psychiatrist terms and need to correct the output format

--- symptom_percent.py
+++ symptom_percent.py
@@ -42,9 +42,9 @@ def correction(sentence,List):
 sentence=input("Describe your problem: ")
 sentence=sentence.lower()
 filepath = 'example2.csv'
-master_list=[]
-Sym_list=[]
-
+symptom_list=[]
+keyword_list=[]
+disease_list=[]
 
 
 with open(filepath) as fp:  
@@ -55,16 +55,27 @@ with open(filepath) as fp:
        strip_list=stripped_line.split(",")
        a=strip_list[0].split(" ")
        b=strip_list[1].split(" ")
-       Sym_list.extend(a)
-       Sym_list.extend(b)
-       master_list.append(strip_list)
+       keyword_list.extend(a)
+       keyword_list.extend(b)
+       symptom_list.append(strip_list)
        line = fp.readline()
 
-Sym_list=list(set(Sym_list))	
-sentence=correction(sentence,Sym_list)
+with open('DiseasesData.csv') as fp:
+	line = fp.readline()
+	while line:
+		stripped_line=line.strip()
+		stripped_line=stripped_line.lower()
+		strip_list=stripped_line.split(",")
+		a=strip_list[0].split(" ")
+		b=strip_list[1].split(" ")
+		keyword_list.extend(a)
+		keyword_list.extend(b)
+		disease_list.append(strip_list)
+		line=fp.readline()
 
+keyword_list=list(set(keyword_list))	
+sentence=correction(sentence,keyword_list)
 
-print(sentence)
 
 ###############################################################################################
 
@@ -90,67 +101,80 @@ for sex in word_tokenize(sentence):
 
 #####################################################################################
 
-sum_list={}
+symptom_dict={}
 dominant_key=''
 psych_present=''
 primary=['sex', 'cancer','abortion']
 primary_indices={}
+flag=0
+disease=""
+specialist=""
+for element in disease_list:
+	if sentence.find(element[1])>=0:
+		flag=1
+		disease=element[1]
+		specialist=element[0]
+		break
+if flag==1:
+	print("You should consult {} for {} ".format(specialist,disease))
 
-for m in primary:
-	if sentence.find(m)>=0:
-		primary_indices[m]=sentence.find(m)
+elif flag==0:	
+	for m in primary:
+		if sentence.find(m)>=0:
+			primary_indices[m]=sentence.find(m)
 
-min_key=''
+	min_key=''
 
-for key,value in primary_indices.items():
-	if primary_indices[key]==min(primary_indices.values()):
-		min_key=key
+	for key,value in primary_indices.items():
+		if primary_indices[key]==min(primary_indices.values()):
+			min_key=key
 
-if min_key=='sex':
-	if gender=='Female' or sentence.find('pregnancy')>=0 or sentence.find('pregnant')>=0 or sentence.find('period')>=0 :
-		if sentence.find('hypertension')>=0 or sentence.find('anxiety')>=0 or sentence.find('stress')>=0 or sentence.find('depression')>=0 :
-			dominant_key='gynaecologist'
-			psych_present='psychiatrist'
-			print('There is a high probability that you should consult a '+dominant_key+' and '+psych_present)
-		else:
-			dominant_key='gynaecologist'
-			print('There is a high probability that you should consult a '+dominant_key)
-	else:
-		if sentence.find('hypertension')>=0 or sentence.find('anxiety')>=0 or sentence.find('stress')>=0 or sentence.find('depression')>=0 :
-			dominant_key='sexologist'
-			psych_present='psychiatrist'
-			print('There is a high probability that you should consult a '+dominant_key+' and '+psych_present)
-		elif sentence.find('period')>=0 or sentence.find('abortion')>=0:
-			dominant_key='gynaecologist'
-			print('There is a high probability that you should consult a '+dominant_key)
-		else:
-			dominant_key='sexologist'
-			print('There is a high probability that you should consult a '+dominant_key)
-
-elif min_key=='cancer':
-	dominant_key='oncologist'
-	print('There is a high probability that you should consult a '+ dominant_key)
-elif sentence.find('abortion')>=0 or sentence.find('period')>=0 :
-	if sentence.find('hypertension')>=0 or sentence.find('anxiety')>=0 or sentence.find('stress')>=0 or sentence.find('depression')>=0 :
-		dominant_key='gynaecologist'
-		psych_present='psychiatrist'
-		print('There is a high probability that you should consult a '+dominant_key+' and '+psych_present)
-	else:
-		dominant_key='gynaecologist'
-		print('There is a high probability that you should consult a '+dominant_key)
-
-else:
-	for element in master_list:
-		if sentence.find(element[1]) >= 0 :
-			if not (element[0] in sum_list.keys()):
-				sum_list[element[0]]=float(element[2])
+	if min_key=='sex':
+		if gender=='Female' or sentence.find('pregnancy')>=0 or sentence.find('pregnant')>=0 or sentence.find('period')>=0 :
+			if sentence.find('hypertension')>=0 or sentence.find('anxiety')>=0 or sentence.find('stress')>=0 or sentence.find('depression')>=0 :
+				dominant_key='gynaecologist'
+				psych_present='psychiatrist'
+				print('There is a high probability that you should consult a '+dominant_key+' and '+psych_present)
 			else:
-				value=sum_list[element[0]]+float(element[2])
-				sum_list[element[0]]=value
-	total=0
-	for value in sum_list.values():
-		total = total + value
-	for key in sum_list.keys():
-		sum_list[key] = sum_list[key]*100/total
-	for key,value in sum_list.items():
-		print("The probability of going to {} is {} percent".format(key,value))
+				dominant_key='gynaecologist'
+				print('There is a high probability that you should consult a '+dominant_key)
+		else:
+			if sentence.find('hypertension')>=0 or sentence.find('anxiety')>=0 or sentence.find('stress')>=0 or sentence.find('depression')>=0 :
+				dominant_key='sexologist'
+				psych_present='psychiatrist'
+				print('There is a high probability that you should consult a '+dominant_key+' and '+psych_present)
+			elif sentence.find('period')>=0 or sentence.find('abortion')>=0:
+				dominant_key='gynaecologist'
+				print('There is a high probability that you should consult a '+dominant_key)
+			else:
+				dominant_key='sexologist'
+				print('There is a high probability that you should consult a '+dominant_key)
+
+	elif min_key=='cancer':
+		dominant_key='oncologist'
+		print('There is a high probability that you should consult a '+ dominant_key)
+	elif sentence.find('abortion')>=0 or sentence.find('period')>=0 :
+		if sentence.find('hypertension')>=0 or sentence.find('anxiety')>=0 or sentence.find('stress')>=0 or sentence.find('depression')>=0 :
+			dominant_key='gynaecologist'
+			psych_present='psychiatrist'
+			print('There is a high probability that you should consult a '+dominant_key+' and '+psych_present)
+		else:
+			dominant_key='gynaecologist'
+			print('There is a high probability that you should consult a '+dominant_key)
+
+
+	else:
+		for element in symptom_list:
+			if sentence.find(element[1]) >= 0 :
+				if not (element[0] in symptom_dict.keys()):
+					symptom_dict[element[0]]=float(element[2])
+				else:
+					value=symptom_dict[element[0]]+float(element[2])
+					symptom_dict[element[0]]=value
+		total=0
+		for value in symptom_dict.values():
+			total = total + value
+		for key in symptom_dict.keys():
+			symptom_dict[key] = symptom_dict[key]*100/total
+		for key,value in symptom_dict.items():
+			print("The probability of going to {} is {} percent".format(key,value))
