382d049b26a5c3c0af9f4f6ea899976573dd430a
Ritesh Ray <rayritesh97@gmail.com>
2018-05-30 15:12:24 +0530

    Add files via upload
    Added documentations and declared variables globally.

--- symptom_percent.py
+++ symptom_percent.py
@@ -1,3 +1,37 @@
+# DOCUMENTATION:
+'''
+The funtions declared are as follows:
+	correction(sentence,List) :- returns a string of corrected words after removing stopwords and implementing autocorrect.
+
+The variables and their datatypes and structures are as follows:
+	stop_words: 		a SET of stopwords in english.
+	sentence: 			a STRING that the user inputs (query).
+	word_tokens: 		a LIST of words that the user enters.
+	symptom_list: 		LIST of symptoms as read from the csv file.
+	disease_list: 		LIST of diseases as read from the csv file.
+	psych_list: 		LIST of diseases that are only treated by a psychiatrist, this will be checked only if none of the other cases match.
+	keyword_list: 		LIST containing the words of all the above 3 lists. If the user entered words match any of the keywords then it will not be autocorrected.
+	male: 				LIST of all the words pointing to male user.
+	female: 			LIST of all the words pointing to female user.
+	symptom_dict: 		DICTIONARY containing the matched list of specialists and their total weights/values.
+	dominany_key: 		empty STRING that will contain the name of specialist if any dominant keyword is present.
+	psych_present: 		empty STRING that will contain psychiatrist if the co-dominant keywords match.
+	primary: 			LIST containing the dominant keywords
+	primary_indices: 	DICTIONARY containing dominant keywords found in the list as keys and their indexes in the sentence as values.
+	disease: 			empty STRING which will store the disease if found in the sentence.
+	specialist:	 		empty STRING which will store the corresponding specialist of the disease found in the sentence.
+	flag: 				0 if no disease found, 1 if a disease is found.
+	min_key: 			minimum indexed dominant keyword.
+	value: 				INT or FLOAT variable storing total weight of a particular specialist based on the number of symptoms pertaining to that specialist. 
+	new_list: 			LIST containing the dictionary values as elements of the list in descending order of weights.
+	print_list: 		subLIST of the new_list containing only the first two elements.
+
+The packages imported are as follows:
+	nltk version 3.3
+	pandas version 0.23.0
+	autocorrect version 0.3.0
+'''
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import pandas as pd
@@ -5,6 +39,7 @@ from autocorrect import spell
 
 #################################################################
 
+# To filter the unwanted words in the sentence and also apply autocorrect
 def correction(sentence,List):
 	stop_words=set(stopwords.words('english'))
 	word_tokens = word_tokenize(sentence)
@@ -39,6 +74,7 @@ def correction(sentence,List):
 
 ########################################################################################################
 
+# To read the files and append the keywords, diseases and symptoms in the respective lists
 sentence=input("Describe your problem: ")
 sentence=sentence.lower()
 filepath = 'example2.csv'
@@ -93,6 +129,7 @@ sentence=correction(sentence,keyword_list)
 
 ###############################################################################################
 
+# To read the file and append the male and female words in their lists accordingly
 male=[]
 female=[]
 filename='MaleFemale.csv'
@@ -115,6 +152,10 @@ for sex in word_tokenize(sentence):
 
 #####################################################################################
 
+# Step1: check if any disease is mentioned, if yes, send it to the specialist. If not, move to step 2
+# Step2: check if anydominant keyword is present, if yes, send it to the specialist. If not, move to step 3
+# Step3: check if symptoms match those in the symptom_list, if yes, calculate accordingly. If not, move to step 4
+# Step4: check if any psychiatric symptoms are present, if yes, refer to a psychiatrist. End.
 symptom_dict={}
 dominant_key=''
 psych_present=''
@@ -130,7 +171,7 @@ for element in disease_list:
 		specialist=element[0]
 		break
 if flag==1:
-	print("You should consult a {}".format(specialist))
+	print("You should consult a {} for {}".format(specialist,disease))
 
 elif flag==0:	
 	for m in primary:
@@ -191,7 +232,6 @@ elif flag==0:
 		for key in symptom_dict.keys():
 			symptom_dict[key] = symptom_dict[key]*100/total
 
-		sortedDict={}
 		new_list=sorted(symptom_dict.items(), key = lambda t: t[1], reverse = True)
 		print_list=[]
 		flag1=0
